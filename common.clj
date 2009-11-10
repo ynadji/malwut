3de184bba1a94e337f9d2f8d45b07b5d649175f8
Yacin Nadji <yacin@gatech.edu>
2009-11-09 22:30:10 -0500

    can parse/read/save clamav logs

--- common.clj
+++ common.clj
@@ -2,6 +2,6 @@
 ;;;; Contains common structs to use
 ;;;; across all of malwut
 
-(def classes ["trojan" "worm" "exploit" "w32" "dialer"])
+(def classes #{"trojan" "worm" "exploit" "w32" "dialer"})
 
-(defstruct maltry :class :name :project :path :md5 :tags)
+(defstruct maltry :class :name :project :path :md5 :tags :varnum)
--- reader.clj
+++ reader.clj
@@ -3,11 +3,34 @@
 		 (:use clojure.contrib.str-utils)
 		 (:use malwut.common))
 
+(defn- get-class
+  "Get class from clamav log output."
+  [tag]
+  (first tag))
+
+(defn- get-name
+  "Get malware name from clamav log output."
+  [tag]
+  (second tag))
+
+(defn- get-varnum
+  "Get malware variant number (not terribly important
+but good to have nonetheless"
+  [tag]
+  (if (empty? tag)
+    0
+    (let [n (first tag)]
+     (if (re-matches #"\d+" n)
+       (Integer. n)
+       (recur (rest tag))))))
+
 (defn- parse-clamav-line
   "Parse single line from clamav log output."
   [line]
-  (let [p (rest (first (re-seq #"(.+?)([a-fA-F0-9]{32}): (.+?) FOUND" line)))]
-    (struct maltry "class" "name" "2010-oakland-malware" (first p) (second p) (nth p 2))))
+  (let [p (rest (first (re-seq #"(.+?)([a-fA-F0-9]{32}): (.+?) FOUND" line)))
+	tags (re-split #"\.|-" (nth p 2))]
+    (struct maltry (get-class tags) (get-name tags) "2010-oakland-malware"
+            (first p) (second p) (set tags) (get-varnum (reverse tags)))))
 
 (defn read-clamav
   "Reads report from Clam AV, parses input. Returned
@@ -17,3 +40,14 @@
 		 10 (drop
 		     3 (read-lines logfile)))]
     (map #(parse-clamav-line %) malware)))
+
+(defn read-and-save-clamav
+  "Reads Clam AV report, dumps it to a nice DB that Clojure can
+read in easily."
+  [logfile dumpdb]
+  (spit dumpdb (pr-str (read-clamav logfile))))
+
+(defn load-clamav
+  "Read in parsed Clam AV report."
+  [dumpdb]
+  (read-string (slurp dumpdb)))
