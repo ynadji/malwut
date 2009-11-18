9db52546cc67a0b0a44e6f7440a0d75f88aa1937
Yacin Nadji <yacin@gatech.edu>
2009-11-18 17:36:22 -0500

    small web-service to query md5s

---
+++ server.clj
@@ -0,0 +1,11 @@
+(clojure.core/ns malwut.server
+                 (:use compojure))
+
+(defroutes my-app
+  (GET "/"
+       (html [:h1 "Hello World"]))
+  (ANY "*"
+       (page-not-found)))
+
+(run-server {:port 8080}
+            "/*" (servlet my-app))
